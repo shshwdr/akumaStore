21b6c566b69587892e5455d82d019f96491ad5ac
shshwdr <cs.vivian04@gmail.com>
2021-12-01 01:29:10 -0600

    customer move

--- Assets/Customer.cs
+++ Assets/Customer.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class Customer : HPCharacterController
 {
 
+    NPCPathFinding pathFinding;
+    public Transform target;
+    protected override void Awake()
+    {
+        base.Awake();
+        pathFinding = GetComponent<NPCPathFinding>();
+    }
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
         CustomerManager.Instance.addCustomer(this);
+
+        pathFinding.setTarget(target);
     }
 
     protected override void Die()
